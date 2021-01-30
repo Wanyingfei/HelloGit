d77003f9ba1af5626501d0030e18658bb44274cf
wanyingfei <1292613988@qq.com>
2021-01-31 00:22:54 +0800

    添加文件

---
+++ src/main/java/HelloGit.java
@@ -0,0 +1,11 @@
+/**
+ * @Classname HelloGit
+ * @Description TODO
+ * @Date 2021-01-31 0:12
+ * @Created by wanyingfei
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
